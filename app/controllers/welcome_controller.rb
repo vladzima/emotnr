afbd5f77f2b5fe1ccf9f9732f7a62cedd9a965aa
Vlad <mail@vladislav.tv>
2016-04-11 23:44:47 +0000

    Lesser count and controller action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    require 'indico'
+    Indico.api_key = "bc5a4dd18dd9575c7a29b61a291df552"
+    
+    @tweets = current_user.twitter.home_timeline(:page => 1, :count => 50)
+    @tweetstext = []
   end
 end
